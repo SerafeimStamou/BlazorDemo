1f2e5871d8e58a5ad0ee0e1d255a7c73ef695e7e
SerafeimStamou <serafeim_stamou86@hotmail.com>
2021-06-03 13:07:28 +0300

    Get Data

--- ApiConnection/ApiCaller.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace ApiConnection
-{
-    public class ApiCaller
-    {
-    }
-}
---
+++ BlazorUI/Pages/PeopleBase.cs
@@ -0,0 +1,42 @@
+﻿using CRUD.Models;
+using Microsoft.AspNetCore.Components;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+namespace BlazorUI.Pages
+{
+    public class PeopleBase : ComponentBase
+    {
+        public List<Person> people { get; set; }
+        public DatabaseResult result { get; set; }
+
+        [Inject]
+        protected  IHttpClientFactory _clientFactory { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            HttpRequestMessage request = new(HttpMethod.Get, "https://localhost:44334/api/Person");
+
+            var client = _clientFactory.CreateClient();
+
+            HttpResponseMessage response = await client.SendAsync(request);
+
+            if (response.IsSuccessStatusCode)
+            {
+                var result = await response.Content.ReadFromJsonAsync<DatabaseResult>();
+ 
+                if (result.Result is not null)
+                {
+                    people = JsonConvert.DeserializeObject<List<Person>>(result.Result.ToString());
+                }
+            }
+            else
+            {
+                result.Message = $"Cannot connect to API: {response.ReasonPhrase}";
+            }
+        }
+    }
+}
